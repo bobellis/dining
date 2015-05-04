56f1d40ba75a235d2829781776d5ba61912713f4
bob ellis & chris sefkin <student@epicodus.com>
2015-05-04 16:11:25 -0700

    semi-working

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant
 
   attr_reader(:name, :cuisine_id)
 
-  define_method(:initialize) do |name, cuisine_id|
+  define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @cuisine_id = attributes.fetch(:cuisine_id)
   end
@@ -12,9 +12,19 @@ class Restaurant
     restaurants = []
     returned_restaurants.each() do |restaurant|
       name = restaurant.fetch('restaurant_name')
-      restaurants.push(Restaurant.new({:name => name}))
+      cuisine_id = restaurant.fetch('cuisine_id')
+      restaurants.push(Restaurant.new({:name => name, :cuisine_id => cuisine_id}))
     end
     restaurants
-
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO restaurants (restaurant_name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_restaurant|
+    self.name().==(another_restaurant.name())
+  end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -6,10 +6,27 @@ require ('spec_helper')
 
 describe('Restaurant') do
 
+  describe("#==") do
+    it("is the same restaurant if it has the same description") do
+      restaurant1 = Restaurant.new({:name => "Joes", :id => nil})
+      restaurant2 = Restaurant.new({:name => "Joes", :id => nil})
+      expect(restaurant1).to(eq(restaurant2))
+    end
+  end
+
     describe('.all') do
       it('starts off empty') do
         expect(Restaurant.all()).to(eq([]))
       end
     end
-  
-  end
+
+    describe("#save") do
+      it("lets you save restaurants to the database") do
+        restaurant = Restaurant.new({:name => "Joes", :cuisine_id => 1})
+        restaurant.save()
+        expect(Restaurant.all()).to(eq([restaurant]))
+      end
+    end
+
+
+end
